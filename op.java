b548614aac99b8881631d4b9fe24f4bf522ff471
ramyapooja <41736592+ramyapooja@users.noreply.github.com>
2018-08-27 22:57:55 +0530

    Create op.java

---
+++ op.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int p=1;
+		for(int i=1;i<=b;i++)
+		{
+			p=p*a;
+		}
+		System.out.println(p%c);
+	}
+}
